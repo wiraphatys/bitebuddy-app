4c034159ec8583831fe12ea6f7321bab25660e62
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-04-03 03:10:53 +0700

    Fixed ReviewRouter invalid imported function

--- backend/routes/ReviewRouter.js
+++ backend/routes/ReviewRouter.js
@@ -1,12 +1,23 @@
 const express = require("express");
-const router = express.Router({mergeParams: true});
+const router = express.Router({ mergeParams: true });
 
-const {getReviews, getReview, createReview, updateReview, deleteReview } = require('../controllers/reviews');
+const {
+    getReviews,
+    getReviewById,
+    createReview,
+    updateReviewById,
+    deleteReviewById
+} = require('../controllers/ReviewController');
 
-const {protect, authorize} = require('../middlewares/AuthMiddleware');
+const { protect, authorize } = require('../middlewares/AuthMiddleware');
 
-router.route('/').get(protect, getReviews).post(protect,authorize('admin','user'), createReview);
+router.route('/')
+    .get(protect, getReviews)
+    .post(protect, authorize('admin', 'user'), createReview);
 
-router.route('/:id').get(protect, getReview).put(protect,authorize('admin','user'), updateReview).delete(protect,authorize('admin','user'), deleteReview);
+router.route('/:id')
+    .get(protect, getReviewById)
+    .put(protect, authorize('admin', 'user'), updateReviewById)
+    .delete(protect, authorize('admin', 'user'), deleteReviewById);
 
 module.exports = router;
