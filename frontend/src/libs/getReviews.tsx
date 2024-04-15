99b3fa77c486e4bbcac3d239b0556bec9473491e
nathaฯ <nathachumsri@gmail.com>
2024-04-16 00:32:28 +0700

    change getReview Path

--- frontend/src/libs/getReviews.tsx
+++ frontend/src/libs/getReviews.tsx
@@ -1,10 +1,15 @@
 import config from "@/utils/config";
 
-export default async function getReviews(id:string) {
+export default async function getReviews(id?:string) {
 
     await new Promise((resolve)=>setTimeout(resolve,1000))
-    const response = await fetch(`${config.api}/restaurants/${id}/reviews`, config.headers());
-
+    let response = null
+    if(id){
+        response = await fetch(`${config.api}/restaurants/${id}/reviews`, config.headers());
+    }else{
+        response = await fetch(`${config.api}/reviews`, config.headers());
+    }
+    
     if (!response.ok) {
         throw new Error('Failed to fetch menu: ' + response.statusText);
     }
