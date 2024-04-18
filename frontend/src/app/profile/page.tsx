efea749912b6d78aab333c51317aadda13d40ef8
Tanathorn Piyachart <tangutang.tnp@gmail.com>
2024-04-18 14:58:10 +0700

    Added style to profile

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -6,75 +6,77 @@ import ReviewSlider from "@/components/ReviewSlider";
 import EditProfile from "@/components/EditProfile";
 import styles from './page.module.css'
 
-interface UserItem{
-    email:string,
-    firstName:string,
-    lastName:string,
-    tel:string,
-    role:string,
-    img:string,
-    _id:string
+interface UserItem {
+  email: string,
+  firstName: string,
+  lastName: string,
+  tel: string,
+  role: string,
+  img: string,
+  _id: string
 }
 const ProfilePage = () => {
-    const [user, setUser] = useState<UserItem>();
-    const [isEditing, setIsEditing] = useState(false);
-  
-    useEffect(() => {
-      const fetchUser = async () => {
-        try {
-          const response = await getUser();
-          setUser(response.data);
-        } catch (error) {
-          console.error('Error fetching user data:', error);
-        }
-      };
-  
-      fetchUser();
-    }, []);
-  
-    const handleEdit = () => {
-      setIsEditing(true);
-    };
-  
-    const handleCloseEdit = () => {
-      setIsEditing(false);
-    };
-  
-    const updateUserProfile = (updatedUser: UserItem) => {
-        setUser(updatedUser);
-        setIsEditing(false);
-      };
-  
-    return (
-      <div className={styles.profileContainer}>
-  <div className={styles.accountHeader}>Account</div>
-  <div className={styles.userCard}>
-    <div className={styles.userContent}>
-      <img
-        src={user?.img ? user.img : '/img/userAnonymous.png'}
-        alt="user picture"
-        width={200}
-        height={200}
-        className={styles.userImage}
-      />
-      <div className={styles.userInfo}>
-        <div className={styles.username}>
-          {user?.firstName} {user?.lastName}
-        </div>
-        <div>role : {user?.role}</div>
-        <div>email : {user?.email}</div>
-        <div>tel : {user?.tel}</div>
-        
-      </div>
-      <button onClick={handleEdit} className={styles.editButton}>Edit</button>
-    </div>
-    {isEditing && user && <EditProfile user={user} onClose={handleCloseEdit} onUpdate={updateUserProfile} />}
-  </div>
-  <div className={styles.reviewHeader}>My Review</div>
-  <ReviewSlider />
-</div>
+  const [user, setUser] = useState<UserItem>();
+  const [isEditing, setIsEditing] = useState(false);
 
-    );
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await getUser();
+        setUser(response.data);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  const handleEdit = () => {
+    setIsEditing(true);
   };
-  
-  export default ProfilePage;
+
+  const handleCloseEdit = () => {
+    setIsEditing(false);
+  };
+
+  const updateUserProfile = (updatedUser: UserItem) => {
+    setUser(updatedUser);
+    setIsEditing(false);
+  };
+
+  return (
+    <div className={styles.profileContainer}>
+      <div className={styles.accountHeader}>Account</div>
+      <div className={styles.userCard}>
+        <div className={styles.userContent}>
+          <img
+            src={user?.img ? user.img : '/img/userAnonymous.png'}
+            alt="user picture"
+            width={200}
+            height={200}
+            className={styles.userImage}
+          />
+          <div className={styles.userInfo}>
+            <div className={styles.username}>
+              {user?.firstName} {user?.lastName}
+            </div>
+            <div>role : {user?.role}</div>
+            <div>email : {user?.email}</div>
+            <div>tel : {user?.tel}</div>
+
+          </div>
+          <button onClick={handleEdit} className={styles.editButton}>Edit</button>
+        </div>
+        <div>
+          {isEditing && user && <EditProfile user={user} onClose={handleCloseEdit} onUpdate={updateUserProfile} />}
+        </div>
+      </div>
+      <div className={styles.reviewHeader}>My Review</div>
+      <ReviewSlider />
+    </div>
+
+  );
+};
+
+export default ProfilePage;
