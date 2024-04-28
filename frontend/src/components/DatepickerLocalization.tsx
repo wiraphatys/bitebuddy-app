a4d69e7c245cbca41c44dc1596f0b24e04742871
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-04-28 13:09:04 +0700

    Changed file permission

--- frontend/src/components/DatepickerLocalization.tsx
+++ frontend/src/components/DatepickerLocalization.tsx
@@ -1,14 +1,14 @@
-import { DateRangePickerLocalization } from '@mui/x-date-pickers';
+// import { DateRangePickerLocalization } from '@mui/x-date-pickers';
 
-const datepickerLocalization: DateRangePickerLocalization = {
-  getFirstMonthLabel: () => 'First Month',
-  getSecondMonthLabel: () => 'Second Month',
-  getWeekdaysLabel: () => ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-  getWeekArray: () => [1, 2, 3, 4, 5, 6, 7],
-  getYearSelectionText: (year: any) => `${year}`,
-  getDatePickerHeaderText: (date:any) => `${date.toLocaleDateString()}`,
-  getLoadingLabel: () => 'Loading...',
-  getNoYearText: () => 'No year',
-};
+// const datepickerLocalization: DateRangePickerLocalization = {
+//   getFirstMonthLabel: () => 'First Month',
+//   getSecondMonthLabel: () => 'Second Month',
+//   getWeekdaysLabel: () => ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+//   getWeekArray: () => [1, 2, 3, 4, 5, 6, 7],
+//   getYearSelectionText: (year: any) => `${year}`,
+//   getDatePickerHeaderText: (date:any) => `${date.toLocaleDateString()}`,
+//   getLoadingLabel: () => 'Loading...',
+//   getNoYearText: () => 'No year',
+// };
 
-export default datepickerLocalization;
+// export default datepickerLocalization;
