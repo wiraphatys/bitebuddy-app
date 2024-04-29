bee006f502338e3be9ea86d7b7bb98f9080bd884
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-04-29 22:08:36 +0700

    Added api_test folder

--- backend/_test_/createReview.test.js
+++ backend/_test_/createReview.test.js
@@ -4,7 +4,7 @@ const { MongoMemoryServer } = require('mongodb-memory-server');
 // Import required models and functions
 const Review = require('../models/ReviewModel');
 const Restaurant = require('../models/RestaurantModel');
-const { createReview } = require('../controllers/ReviewController');
+const { createReview } = require('../api_test/CreateReview');
 
 describe('reviewController.createReview', () => {
   let mongoServer;
