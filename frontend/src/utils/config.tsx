37c49e2b5e1cd48ab31099ac9aef7e4438d4e0c6
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-05-01 11:33:19 +0700

    Updated config

--- frontend/src/utils/config.tsx
+++ frontend/src/utils/config.tsx
@@ -1,17 +1,15 @@
 const config = {
-    api: "http://localhost:4000/api/v1",
+    api: "https://bitebuddy-api.vercel.app/api/v1",
     tokenName: 'token',
     headers: () => {
         return {
             headers: {
                 'Authorization': 'Bearer ' + localStorage.getItem('token'),
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': "http://localhost:4000/"
+                'Access-Control-Allow-Origin': "https://bitebuddy-api.vercel.app/"
             }
         }
     }
 }
 
 export default config
-
-// https://bitebuddy-api.vercel.app/api/v1
